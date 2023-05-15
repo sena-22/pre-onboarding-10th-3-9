16baeefb12a5e0f5eb1877cd4006996f53e516c5
sena-22 <senika_@naver.com>
2023-05-15 21:56:48 +0900

    feat:type 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,30 @@
 const Header = () => {
   const headerStyle = {
-    padding: "20px 0",
-    lineHeight: "1.5em",
-  }
+    padding: '20px 0',
+    lineHeight: '1.5em',
+  };
 
   enum TextAlign {
-    left = "left",
-    center = "center",
-    right = "right",
-    justify = "justify",
+    left = 'left',
+    center = 'center',
+    right = 'right',
+    justify = 'justify',
   }
 
   const titleStyle = {
-    fontSize: "6rem",
-    fontWeight: "600",
-    marginBottom: "2rem",
-    lineHeight: "1em",
-    color: "#ececec",
+    fontSize: '6rem',
+    fontWeight: '600',
+    marginBottom: '2rem',
+    lineHeight: '1em',
+    color: '#ececec',
     textAlign: TextAlign.center as TextAlign | undefined,
-  }
+  };
 
   return (
     <header style={headerStyle}>
       <h1 style={titleStyle}>Toodos</h1>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
